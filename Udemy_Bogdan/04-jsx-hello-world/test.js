f389ae7f9d7c0e983470c37b8e7825c17ff5bc0a
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2022-12-17 22:41:44 +0100

    .

---
+++ Udemy_Bogdan/04-jsx-hello-world/test.js
@@ -0,0 +1,6 @@
+
+function HelloReact() {
+  return (/*#__PURE__*/React.createElement("h1", null, "Hello React")
+    // 1 element /*#__PURE__*/React.createElement("h1", null, "Hello React")
+  );
+}
