e33a1f23282791b2d074cff2c82f5ba8883914ef
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-01-29 21:47:56 +0100

    webstorm

--- React-training/04-cinema/src/components/Titre.js
+++ React-training/04-cinema/src/components/Titre.js
@@ -7,5 +7,4 @@ function Titre(){
     )
 
 }
-
 export default Titre;
--- React-training/07-films/src/App.js
+++ React-training/07-films/src/App.js
@@ -1,65 +1,64 @@
-
 import Film from "./Films";
 
 
 const FilmArray = [
-	{
-		video:
-			"/Users/vladimirsinkevitch/Desktop/React/filme-a-la-affiche/src/IMG_7604.MP4",
-		title: "House of dragon",
-		resume: "Cool",
-		date: "04/10/2022",
-		heure: "19h00, 21h30, 23h00",
-	},
-	{
-		video:
-			"/Users/vladimirsinkevitch/Desktop/React/filme-a-la-affiche/src/IMG_7604.MP4",
-		title: "Games of Throne",
-		resume: "Cool",
-		date: "04/10/2022",
-		heure: "19h00, 21h30, 23h00",
-	},
-	{
-		video:
-			"/Users/vladimirsinkevitch/Desktop/React/filme-a-la-affiche/src/IMG_7604.MP4",
-		title: "Rick and Morty",
-		resume: "Cool",
-		date: "04/10/2022",
-		heure: "19h00, 21h30, 23h00",
-	},
-	{
-		video:
-			"/Users/vladimirsinkevitch/Desktop/React/filme-a-la-affiche/src/IMG_7604.MP4",
-		title: "007",
-		resume: "Cool",
-		date: "04/10/2022",
-		heure: "19h00, 21h30, 23h00",
-	},
-	{
-		video:
-			"/Users/vladimirsinkevitch/Desktop/React/filme-a-la-affiche/src/IMG_7604.MP4",
-		title: "Deadpool 3",
-		resume: "Cool",
-		date: "04/10/2022",
-		heure: "19h00, 21h30, 23h00",
-	},
+  {
+    video:
+      "/Users/vladimirsinkevitch/Desktop/React/filme-a-la-affiche/src/IMG_7604.MP4",
+    title: "House of dragon",
+    resume: "Cool",
+    date: "04/10/2022",
+    heure: "19h00, 21h30, 23h00",
+  },
+  {
+    video:
+      "/Users/vladimirsinkevitch/Desktop/React/filme-a-la-affiche/src/IMG_7604.MP4",
+    title: "Games of Throne",
+    resume: "Cool",
+    date: "04/10/2022",
+    heure: "19h00, 21h30, 23h00",
+  },
+  {
+    video:
+      "/Users/vladimirsinkevitch/Desktop/React/filme-a-la-affiche/src/IMG_7604.MP4",
+    title: "Rick and Morty",
+    resume: "Cool",
+    date: "04/10/2022",
+    heure: "19h00, 21h30, 23h00",
+  },
+  {
+    video:
+      "/Users/vladimirsinkevitch/Desktop/React/filme-a-la-affiche/src/IMG_7604.MP4",
+    title: "007",
+    resume: "Cool",
+    date: "04/10/2022",
+    heure: "19h00, 21h30, 23h00",
+  },
+  {
+    video:
+      "/Users/vladimirsinkevitch/Desktop/React/filme-a-la-affiche/src/IMG_7604.MP4",
+    title: "Deadpool 3",
+    resume: "Cool",
+    date: "04/10/2022",
+    heure: "19h00, 21h30, 23h00",
+  },
 ];
 
 function App() {
-	return (
-		<div className="App">
-			{FilmArray.map((filmObject) => {
-				return (
-					<Film
-						video={filmObject.video}
-						title={filmObject.title}
-						resume={filmObject.resume}
-						heure={filmObject.heure}
-					/>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div className="App">
+      {FilmArray.map((filmObject) => {
+        return (
+          <Film
+            video={filmObject.video}
+            title={filmObject.title}
+            resume={filmObject.resume}
+            heure={filmObject.heure}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
