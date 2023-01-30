ebc230a464fe56869f700d5eb79e690e5c1f5df2
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-01-30 21:47:00 +0100

    react Udemy

--- Udemy_Bogdan/10-conditional-rendering/src/components/PetInfo.js
+++ Udemy_Bogdan/10-conditional-rendering/src/components/PetInfo.js
@@ -4,6 +4,13 @@ function PetInfo(props) {
   const {animal, age, hasPet} = props;
   const text = hasPet ? `My ${animal} is ${age} years old` : 'I don\'t have an animal';
   return <h1>{text}</h1>;
+
+//   jsx code
+  /*
+  return hasPet
+  ? <h1>{`My ${animal} is ${age} years old`}</h1>
+  : <h1>"I don't have an animal"</h1>
+   */
 }
 
 export default PetInfo;
