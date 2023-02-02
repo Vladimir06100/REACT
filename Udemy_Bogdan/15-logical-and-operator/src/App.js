934ca002ab3a00e6461596b1453242a343760750
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-02-02 21:05:53 +0100

    .

--- Udemy_Bogdan/15-logical-and-operator/src/App.js
+++ Udemy_Bogdan/15-logical-and-operator/src/App.js
@@ -8,8 +8,8 @@ function App() {
   const [count, setCount] = useState(0);
   const incrementCount = () => setCount(count + 1);
   const resetCount = () => {
-    setCount(0)
-  }
+    setCount(0);
+  };
 
   return (
     <div className="App">
