03fb3aa78cb2efc6a3fca792a655e1b51d501fc7
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-02-02 22:58:23 +0100

    user

--- Udemy_Bogdan/17-inconrtolled-input/src/App.js
+++ Udemy_Bogdan/17-inconrtolled-input/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-
+      <Login />
     </div>
   );
 }
--- Udemy_Bogdan/17-inconrtolled-input/src/components/Login.js
+++ Udemy_Bogdan/17-inconrtolled-input/src/components/Login.js
@@ -1,10 +1,36 @@
 function Login() {
+  function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const userData = {
+      username: event.target.username.value,
+      password: event.target.password.value,
+    };
+    console.log(userData);
+    alert(JSON.stringify(userData));
+  }
+
   return (
-    <form>
-      <label>
-        Username: <input type="text" name="username" />
-      </label>
-    </form>
+    <>
+      <h1>Login Form</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          Username:{' '}
+          <input
+            type="text"
+            name="username"
+          />
+        </label>
+        <label>
+          Password:{' '}
+          <input
+            type="password"
+            name="password"
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </>
   );
 }
 export default Login;
