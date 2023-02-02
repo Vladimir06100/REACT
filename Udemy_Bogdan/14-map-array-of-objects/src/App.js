01a8bd846c06630d39821a8211ace20b960e3fcb
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-02-02 18:38:33 +0100

    s operation

--- Udemy_Bogdan/14-map-array-of-objects/src/App.js
+++ Udemy_Bogdan/14-map-array-of-objects/src/App.js
@@ -6,14 +6,15 @@ function App() {
   return (
     <div className="App">
       {persons.map((person) => {
-        const { id, firstName, lastName, email, img } = person;
+        // const { id, firstName, lastName, email, img } = person;
         return (
           <Person
-            id={id}
-            firstName={firstName}
-            lastName={lastName}
-            email={email}
-            img={img}
+            {...person}
+            // id={id}
+            // firstName={firstName}
+            // lastName={lastName}
+            // email={email}
+            // img={img}
           />
         );
       })}
