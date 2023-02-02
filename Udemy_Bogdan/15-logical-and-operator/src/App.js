dfcd45d908d02f465608cda09b5703521ca4e907
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-02-02 20:27:06 +0100

    15

---
+++ Udemy_Bogdan/15-logical-and-operator/src/App.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './App.css';
+import Button from './components/Button';
+import Counter from './components/Counter';
+
+function App() {
+  console.log('App rendered');
+  const [count, setCount] = useState(0);
+  const incrementCount = () => setCount(count + 1);
+
+  return (
+    <div className="App">
+      <Counter count={count} />
+      <Button onClick={incrementCount} />
+      <Button onClick={incrementCount} />
+      <Button onClick={incrementCount} />
+      <Button onClick={incrementCount} />
+    </div>
+  );
+}
+
+export default App;
