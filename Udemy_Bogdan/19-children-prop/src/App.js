6a5626ec98a5b77cd6ad3921aa87d31d4a1ceef7
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-02-03 09:06:45 +0100

    children

--- Udemy_Bogdan/19-children-prop/src/App.js
+++ Udemy_Bogdan/19-children-prop/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Wrapper from './components/Wrapper';
 
 function App() {
   return (
     <div className="App">
-
+      <Wrapper>
+        <h2>Text inside of the Wrapper</h2>
+      </Wrapper>
     </div>
   );
 }
---
+++ Udemy_Bogdan/19-children-prop/src/components/Wrapper.js
@@ -0,0 +1,9 @@
+function Wrapper(props) {
+  console.log(props);
+  return (
+    <div>
+      <h1>Hello from Wrapper</h1>
+    </div>
+  );
+}
+export default Wrapper;
