8bb3f0553462e2fa5d049ce267a18434f1626bcf
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-02-03 09:38:35 +0100

    ..

--- Udemy_Bogdan/19-children-prop/src/App.js
+++ Udemy_Bogdan/19-children-prop/src/App.js
@@ -4,8 +4,17 @@ import Wrapper from './components/Wrapper';
 function App() {
   return (
     <div className="App">
-      <Wrapper>
+      <Wrapper color="lightblue">
         <h2>Text inside of the Wrapper</h2>
+        <button>Click me!</button>
+      </Wrapper>
+      <Wrapper color="lightgreen">
+        <h2>Another Text</h2>
+        <p>Some Description</p>
+        <input
+          type="text"
+          placeholder="Enter value"
+        />
       </Wrapper>
     </div>
   );
--- Udemy_Bogdan/19-children-prop/src/components/Wrapper.js
+++ Udemy_Bogdan/19-children-prop/src/components/Wrapper.js
@@ -1,9 +1,11 @@
 function Wrapper(props) {
   console.log(props);
-  return (
-    <div>
-      <h1>Hello from Wrapper</h1>
-    </div>
-  );
+  const style = {
+    backgroundColor: props.color,
+    width: '250px',
+    padding: '20px',
+    margin: '20px auto',
+  };
+  return <div style={style}>{props.children}</div>;
 }
 export default Wrapper;
