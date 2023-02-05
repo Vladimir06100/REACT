45f8b1fc2fd2af58a0dfbe9ba23242d0aafa0660
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-02-05 22:07:45 +0100

    isLoading

--- Udemy_Bogdan/21-jsonplaceholder-posts/src/components/Posts.js
+++ Udemy_Bogdan/21-jsonplaceholder-posts/src/components/Posts.js
@@ -2,21 +2,35 @@ import { useEffect, useState } from 'react';
 import Post from './Post';
 function Posts() {
   const [posts, setPosts] = useState([]);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then((res) => res.json())
       .then((posts) => setPosts(posts))
-      .catch((error) => console.log(error.message));
+      .catch((error) => setError(error.message))
+      .finally(() => setIsLoading(false));
   }, []);
+
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  }
 
   return (
     <div>
-      {posts.map((post) => (
-        <Post
-          key={post.id}
-          {...post}
-        />
-      ))}
+      <h1>Posts</h1>
+      <hr />
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        posts.map((post) => (
+          <Post
+            key={post.id}
+            {...post}
+          />
+        ))
+      )}
     </div>
   );
 }
