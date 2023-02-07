82aee03dd80c2ecca35b89066ab0bd1340a03982
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-02-07 12:22:03 +0100

    module style

--- Udemy_Bogdan/23-css-modules/src/App.js
+++ Udemy_Bogdan/23-css-modules/src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
       <Info />
+      <div className="info">
+        <h1>App component heading</h1>
+        <button className="my-button">App component Button</button>
+      </div>
     </div>
   );
 }
--- Udemy_Bogdan/23-css-modules/src/components/Info.js
+++ Udemy_Bogdan/23-css-modules/src/components/Info.js
@@ -1,9 +1,14 @@
+import './Info.css';
+import styles from './Info.module.css';
+
+console.log(styles);
+
 function Info() {
   return (
-    <>
+    <div className={styles.info}>
       <h1>Hello from the Info component</h1>
-      <button>Click me!</button>
-    </>
+      <button className="my-button">Click me!</button>
+    </div>
   );
 }
 export default Info;
