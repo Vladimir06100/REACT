82efc20ab5e77bb4d6cafb47d43664125b66ebfe
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-02-07 10:49:23 +0100

    ..

--- Udemy_Bogdan/22-useEffect-async-await/src/components/Posts.js
+++ Udemy_Bogdan/22-useEffect-async-await/src/components/Posts.js
@@ -6,13 +6,38 @@ function Posts() {
   const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(true);
 
+  // useEffect(async () => {
+  //   try {
+  //     const res = await fetch(API_URL);
+  //     const posts = await res.json();
+  //     setPosts(posts);
+  //   } catch (error) {
+  //     setError(error.message);
+  //   }
+  //   setIsLoading(false);
+  // }, []);
+
   useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then((posts) => setPosts(posts))
-      .catch((error) => setError(error.message))
-      .finally(() => setIsLoading(false));
+    async function fetchData() {
+      try {
+        const res = await fetch(API_URL);
+        const posts = await res.json();
+        setPosts(posts);
+      } catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+    }
+    fetchData();
   }, []);
+
+  // useEffect(() => {
+  //   fetch(API_URL)
+  //     .then((res) => res.json())
+  //     .then((posts) => setPosts(posts))
+  //     .catch((error) => setError(error.message))
+  //     .finally(() => setIsLoading(false));
+  // }, []);
 
   if (error) {
     return <h1>Error: {error}</h1>;
