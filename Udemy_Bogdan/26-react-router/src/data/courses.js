9d6a136f3462d694ba02b31c4cd072333475632e
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-02-14 18:33:11 +0100

    ..

---
+++ Udemy_Bogdan/26-react-router/src/data/courses.js
@@ -0,0 +1,22 @@
+const courses = [
+  {
+    title: 'Complete React Guide',
+    slug: 'react',
+    id: 257,
+  },
+  {
+    title: 'Complete JavaScript Guide',
+    slug: 'javascript',
+    id: 573,
+  },
+  {
+    title: 'Complete Python Guide',
+    slug: 'python',
+    id: 872,
+  },
+  {
+    title: 'Complete Node.js Guide',
+    slug: 'node-js',
+    id: 361,
+  },
+];
