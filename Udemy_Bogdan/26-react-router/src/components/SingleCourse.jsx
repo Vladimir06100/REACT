e14b4a73101a856c3f36064f3133c08d115a2264
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-02-15 15:52:19 +0100

    относительная ссылка

--- Udemy_Bogdan/26-react-router/src/components/SingleCourse.jsx
+++ Udemy_Bogdan/26-react-router/src/components/SingleCourse.jsx
@@ -1,5 +1,18 @@
+import { Link, useParams } from 'react-router-dom';
+import courses from '../data/courses';
+
 const SingleCourse = () => {
-  return <h1>Single Course info</h1>;
+  const params = useParams();
+  const course = courses.find((course) => course.slug === params.slug);
+  return (
+    <>
+      <h1>{course.title}</h1>
+      <h2>{course.slug}</h2>
+      <h3>{course.id}</h3>
+      {/* либо to=".." relative="path"  перейти на уровень выше  */}
+      <Link to="/courses">All courses</Link>
+    </>
+  );
 };
 
 export default SingleCourse;
