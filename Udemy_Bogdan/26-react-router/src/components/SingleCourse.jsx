ce6dc4099317bd784a9cf90556761a74d5c7e52a
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-02-16 21:13:23 +0100

    NotFound

--- Udemy_Bogdan/26-react-router/src/components/SingleCourse.jsx
+++ Udemy_Bogdan/26-react-router/src/components/SingleCourse.jsx
@@ -1,9 +1,13 @@
 import { Link, useParams } from 'react-router-dom';
 import courses from '../data/courses';
+import NotFound from './NotFound'
 
 const SingleCourse = () => {
   const params = useParams();
   const course = courses.find((course) => course.slug === params.slug);
+  if (!course) {
+    return <NotFound />;
+  }
   return (
     <>
       <h1>{course.title}</h1>
